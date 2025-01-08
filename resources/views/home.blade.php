c3724ecab04ee3edc9f012d68715debaa466c179
oinkni <daniel.georza@yahoo.com>
2025-01-08 10:33:40 +0200

    fixat path pe butonul de join

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,6 @@
     <p class="lead text-color">Empowering women in financial technology through community and collaboration.</p>
     <hr class="my-4">
     <p text-color>Join our community of professional women in FinTech.</p>
-    <a class="btn btn-primary btn-lg" href="add_member.php" role="button">Join Now</a>
+    <a class="btn btn-primary btn-lg" href="{{ route('members.create') }}" role="button">Join Now</a>
 </div>
 @endsection
